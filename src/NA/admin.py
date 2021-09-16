dde881f7fb18e904600eacd0d622314ac3b6d893
Petryk <57829569+Sasha-hk@users.noreply.github.com>
2021-09-15 22:45:37 -0500

    Created models to NA

--- src/NA/admin.py
+++ src/NA/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Advertisment, Event, News
 
-# Register your models here.
+admin.site.register(Advertisment)
+admin.site.register(Event)
+admin.site.register(News)
--- src/NA/models.py
+++ src/NA/models.py
@@ -1,3 +1,98 @@
 from django.db import models
+from string import ascii_letters
+from random import choice
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField('Назва новини', max_length=100)
+    description = models.TextField('Опис новини')
+    baner = models.ImageField('Батер', upload_to='NA/news/', blank=True) 
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    slug = models.SlugField(max_length=11, default='', blank=True, editable=False) 
+
+    def generate_slug(self):
+        slug_str = '' 
+
+        if self.slug == '': 
+            for i in range(0, News._meta.get_field('slug').max_length):
+                slug_str += choice(ascii_letters) 
+            while True: 
+                if News.objects.filter(slug = slug_str).exists():  
+                    for i in range(0, self.slug.max_length): 
+                        slug_str += choice(ascii_letters)
+            
+                else: 
+                    self.slug = slug_str
+                    break
+
+    def save(self, *args, **kwargs): 
+        self.generate_slug()  
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.title)
+
+class Advertisment(models.Model):
+    title = models.CharField('Назва оголошення', max_length=100)
+    description = models.TextField('Опис оголошення')
+    baner = models.ImageField('Батер', upload_to='NA/advertisment/', blank=True) 
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    slug = models.SlugField(max_length=11, default='', blank=True, editable=False) 
+
+    def generate_slug(self):
+        slug_str = '' 
+
+        if self.slug == '': 
+            for i in range(0, Advertisment._meta.get_field('slug').max_length):
+                slug_str += choice(ascii_letters) 
+            while True: 
+                if Advertisment.objects.filter(slug = slug_str).exists():  
+                    for i in range(0, self.slug.max_length): 
+                        slug_str += choice(ascii_letters)
+            
+                else: 
+                    self.slug = slug_str
+                    break
+
+    def save(self, *args, **kwargs): 
+        self.generate_slug()  
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.title)
+
+class Event(models.Model):
+    title = models.CharField('Назва події', max_length=100)
+    description = models.TextField('Опис події')
+    baner = models.ImageField('Батер', upload_to='NA/envent/', blank=True)
+    event_date = models.DateTimeField('Дата проведення')
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    slug = models.SlugField(max_length=11, default='', blank=True, editable=False) 
+
+    def generate_slug(self):
+        slug_str = '' 
+
+        if self.slug == '': 
+            for i in range(0, Event._meta.get_field('slug').max_length):
+                slug_str += choice(ascii_letters) 
+            while True: 
+                if Event.objects.filter(slug = slug_str).exists():  
+                    for i in range(0, self.slug.max_length): 
+                        slug_str += choice(ascii_letters)
+            
+                else: 
+                    self.slug = slug_str
+                    break
+
+    def save(self, *args, **kwargs): 
+        self.generate_slug()  
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.title)

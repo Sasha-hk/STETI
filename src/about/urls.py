fef67952cc059587ef2a71a242b3802f3c7c04a9
Petryk <57829569+Sasha-hk@users.noreply.github.com>
2021-09-21 18:43:01 -0500

    Ready contacts page

--- src/about/urls.py
+++ src/about/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('contacts/', views.Contact.as_view(), name='contacts')
+    path('contacts/', views.Contact.as_view() , name='contacts')
 ]
--- src/about/views.py
+++ src/about/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import ContactsFace, ContactsPhoneNumbers 
 
-class Contact():
+class Contact(View):
     def get(self, request):
-        pass
+        context = {
+            'faces': ContactsFace.objects.order_by('-id'),
+            'phone_numbers': ContactsPhoneNumbers.objects.order_by('-id'),
+        }
+        return render(request, 'about/contacts.html', context)

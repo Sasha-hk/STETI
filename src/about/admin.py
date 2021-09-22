b213b3f0d0f5297959ad8ae1d4185c785466d5a4
Petryk <57829569+Sasha-hk@users.noreply.github.com>
2021-09-21 20:01:57 -0500

    Ready gallery page

--- src/about/admin.py
+++ src/about/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import ContactsFace, ContactsPhoneNumbers 
+from .models import ContactsFace, ContactsPhoneNumbers, Gallery, GalleryCategory, GalleryCategoryImage
 
 admin.site.register(ContactsPhoneNumbers)
 admin.site.register(ContactsFace)
+admin.site.register(Gallery)
+admin.site.register(GalleryCategory)
+admin.site.register(GalleryCategoryImage)
--- src/about/views.py
+++ src/about/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from .models import ContactsFace, ContactsPhoneNumbers 
+from .models import ContactsFace, ContactsPhoneNumbers, Gallery, GalleryCategory
 
 class Contact(View):
     def get(self, request):
@@ -9,3 +9,21 @@ class Contact(View):
             'phone_numbers': ContactsPhoneNumbers.objects.order_by('-id'),
         }
         return render(request, 'about/contacts.html', context)
+
+class GalleryList(View):
+    def get(self, request):
+        context = {
+            'categorys': GalleryCategory.objects.order_by('-id'),
+        }
+
+        return render(request, 'about/gallery-list.html', context)
+
+class GalleryDetail(View):
+    def get(self, request, id):
+        context = {
+            'imgs': GalleryCategory.objects.get(id = id), 
+        }
+
+        return render(request, 'about/gallery-details.html', context)
+
+    

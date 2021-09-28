662e91fd3723d25fe1ef02d9a7b99867c3429b7a
Petryk Oleksandr <sachapos50@gmail.com>
2021-09-28 23:22:02 +0300

    Some changes in Base class JS and Ready Pages for student and entrant

--- src/study/urls.py
+++ src/study/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('library/', views.Library.as_view(), name='library'),
-    path('<str:slug>/', views.LibraryDetail.as_view(), name='library_detail'),
+    path('library/<str:slug>/', views.LibraryDetail.as_view(), name='library_detail'), 
+    path('for-student/', views.ForStudentView.as_view(), name="for_student"),
+    path('for-entrant/', views.ForEntrantView.as_view(), name="for_entrant"),
 ]

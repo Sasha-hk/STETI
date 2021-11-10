958f9794291b7f078dc0ed6541fac86b9248c8bd
Sasha-hk <sachapos50@gmail.com>
2021-11-10 11:14:22 +0200

    Add depth level for news and announcements

--- backend/NAE/serializers.py
+++ backend/NAE/serializers.py
@@ -13,21 +13,25 @@ class AnnouncementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Announcement
         fields = "__all__"
+        depth = 1
 
 
 class ImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Images
         fields = "__all__"
+        depth = 1
 
 
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
         fields = "__all__"
+        depth = 1
 
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = "__all__"
+        depth = 1

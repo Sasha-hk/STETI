5d4da5bca7536a677377818922b8bc765a3297bc
Sasha-hk <sachapos50@gmail.com>
2021-11-15 20:22:54 +0200

    Basckend: added slash int the end of detail pages

--- backend/study/urls.py
+++ backend/study/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('departments/', views.DepartmentView.as_view()),
 
     path('for-entrants/', views.ForEntrantView.as_view()),
-    path('for-entrants/<str:slug>', views.ForEntrantDetailsView.as_view()),
+    path('for-entrants/<str:slug>/', views.ForEntrantDetailsView.as_view()),
 
     path('for-students/', views.ForStudentView.as_view()),
-    path('for-students/<str:slug>', views.ForStudentViewDetailsView.as_view()),
+    path('for-students/<str:slug>/', views.ForStudentViewDetailsView.as_view()),
 
     path('library/', views.LibraryCategoryView.as_view()),
-    path('library/<str:slug>', views.LibraryItemView.as_view()),    
+    path('library/<str:slug>/', views.LibraryItemView.as_view()),    
 ]

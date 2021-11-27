5154ccd4eaf74efbf6cbca510913ea2ac6d3017a
Sasha-hk <sachapos50@gmail.com>
2021-11-27 19:05:35 +0200

    [Backend] Resolve #75

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,26 +1,16 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
-
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
+
+urlpatterns = [
+    path('about/', include('about.urls')),
+    path('admin/', admin.site.urls),
+    path('nae/', include('NAE.urls')),
+    path('study/', include('study.urls')),
+]
 
 if settings.DEBUG:
-    urlpatterns = [
-        path('api/about/', include('about.urls')),
-        path('api/admin/', admin.site.urls),
-        path('api/nae/', include('NAE.urls')),
-        path('api/study/', include('study.urls')),
-    ] 
-
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)                                                                                                                                                  
-
-else: 
-    urlpatterns = [
-        path('about/', include('about.urls')),
-        path('admin/', admin.site.urls),
-        path('nae/', include('NAE.urls')),
-        path('study/', include('study.urls')),
-    ]
-
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
